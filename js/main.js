d93ac763870173f04e4128fdb053349c079be035
David O'Dey <david.o'dey@pearson.com>
2016-07-19 15:40:08 -0700

    complete

--- js/main.js
+++ js/main.js
@@ -14,9 +14,64 @@ $(document).ready(function(){
            $(this).addClass('wrong');
            $(this).find('i').addClass('fa-times-circle').removeClass('fa-circle-thin');
        }
-    })
+        e.preventDefault();
+    });
+
 
     $('.dropdown-container').on('click', 'button', function(){
         $('.dropdown-container').find('ul').toggleClass('hide');
+        e.preventDefault();
     });
+
+
+    $('header').on('click', '.menu-btn', function(){
+        $('.menu-overlay').removeClass('hide').slideDown();
+        e.preventDefault();
+    });
+
+
+    $('#mobile-menu').on('click', 'button', function(){
+      $('.menu-overlay').addClass('hide');
+        e.preventDefault();
+    });
+
+    $('.back-btn').on('click', function(){
+        $('.construction-overlay').addClass('hide');
+        e.preventDefault();
+    });
+
+    $('.container').on('click', '.card-link', function(e){
+        if ($(this).hasClass('calculate')) {
+            $('.calculate-overlay').removeClass('hide');
+        } else if ($(this).hasClass('howto')) {
+            $('.howto-overlay').removeClass('hide');
+        } else if ($(this).hasClass('problem')) {
+            return;
+        } else {
+            $('.construction-overlay').removeClass('hide');
+        }
+        e.preventDefault();
+
+    });
+
+    $('.close-btn').on('click', function(){
+        $('.calculate-overlay').addClass('hide');
+        e.preventDefault();
+    });
+
+    $('.close-howto-btn').on('click', function(){
+        $('.howto-overlay').addClass('hide');
+        e.preventDefault();
+    });
+
+    $('.menu-overlay').on('click', 'a', function(){
+        if ($(this).hasClass('home')) {
+            return;
+        } else {
+            $('.menu-overlay').addClass('hide');
+            $('.construction-overlay').removeClass('hide');
+        }
+
+    });
+
 });
